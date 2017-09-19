088cbaf3bb5bc05fb2c29f5ea1f3a9ab9f9a4752
hukoru <code437@gmail.com>
2017-09-19 23:07:35 +0900

    Initial commit

---
+++ src/main/java/com/ondot/ezsetup/EzsetupApplication.java
@@ -0,0 +1,12 @@
+package com.ondot.ezsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzsetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzsetupApplication.class, args);
+	}
+}
