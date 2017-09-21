acca74f795de5fb20e959b1001d6f836dababcc2
hukoru <code437@gmail.com>
2017-09-22 00:25:03 +0900

    first commit

--- src/main/java/com/ondot/ezsetup/bootstrap/DevBootstrap.java
+++ src/main/java/com/ondot/ezsetup/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.ondot.ezsetup.bootstrap;
 
 import com.ondot.ezsetup.model.Author;
 import com.ondot.ezsetup.model.Book;
+import com.ondot.ezsetup.model.Publisher;
 import com.ondot.ezsetup.repositories.AuthorRepository;
 import com.ondot.ezsetup.repositories.BookRepository;
+import com.ondot.ezsetup.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,9 +30,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private void initData(){
 
+        Publisher publisher = new Publisher();
+        publisher.setName("foo");
+
+        publisherRepository.save(publisher);
+
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Haber Colltin");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -37,7 +46,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
---
+++ src/main/java/com/ondot/ezsetup/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package com.ondot.ezsetup.repositories;
+
+import com.ondot.ezsetup.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository  extends CrudRepository<Publisher, Long>{
+}
